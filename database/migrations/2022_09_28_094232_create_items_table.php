4fe80e0151ab8978c5b6bdb5ec94b02d4f40c7da
Your Name <you@example.com>
2022-09-28 14:23:11 +0300

    setup tabels , crud and spatia

---
+++ database/migrations/2022_09_28_094232_create_items_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('items', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('category_id')->constrained()->cascadeOnDelete();
+            $table->string('name')->unique();
+            $table->string('code');
+            $table->integer('min');
+            $table->integer('amount');
+            $table->float('price');
+            $table->string('image');
+            $table->boolean('active')->default(false);
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('items');
+    }
+}
